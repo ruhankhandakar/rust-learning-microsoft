1152b8b085673d288cad57777c97030c6f1e430f
Ruhan Khandakar <khandakaruhan@gmail.com>
2026-03-26 05:09:29 +0600

    feat: add @vercel/speed-insights package and integrate SpeedInsights component into layout for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -52,6 +53,7 @@ export default function RootLayout({
         <ThemeProvider>
           <ProgressProvider>
             {children}
+            <SpeedInsights />
             <Analytics />
             <OfflineIndicator />
             <ServiceWorkerRegister />
