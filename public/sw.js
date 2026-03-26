b41c0e0bdc68de53b6de9cc4ecadb40654704769
Ruhan Khandakar <khandakaruhan@gmail.com>
2026-03-26 16:03:23 +0600

    4.2.3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rust-training-4.2.2";
+const CACHE_NAME = "rust-training-4.2.3";
 const CONTENT_HASH_URL = "/content-hash.json";
 
 const PRECACHE_URLS = ["/", "/~offline", "/manifest.webmanifest", "/icon.svg"];
